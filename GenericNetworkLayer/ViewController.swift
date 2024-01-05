35a2daf55f6b83a894c50f36e7fdf748868f3fb4
Soner Demirci <soner.demirci@tein.com.tr>
2024-01-05 08:07:25 +0300

    Initial Commit

---
+++ GenericNetworkLayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GenericNetworkLayer
+//
+//  Created by Soner Demirci on 5.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
